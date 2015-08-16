0f80f954c87ed7cd652086609af5e1f7fe225cc0
audahadi <wan.audahadi@gmail.com>
2015-08-16 23:45:46 +0800

    Revert "Add pick-up CancroDoze package for doze mode"
    This reverts commit f6d7e2c2e13fae1e95611eedecef799880cbc4f2.
    
    Change-Id: I76be0d704d374d968ec4aeca49483460e9bc7695

--- cancro.mk
+++ cancro.mk
@@ -267,10 +267,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.qualcomm.sns.daemon=w \
     debug.qualcomm.sns.libsensor1=w
 
-#Doze Mode
-PRODUCT_PACKAGES += \
-    HammerheadDoze
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     make_ext4fs \
